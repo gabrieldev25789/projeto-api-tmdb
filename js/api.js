be82d9860d4139c2f6ae489c3381dab670748e86
Gabriel <gabrielsb1090@gmail.com>
2025-11-09 19:45:48 -0300

    criando e adaptando evento de select para alternar entre serie e filme

--- js/api.js
+++ js/api.js
@@ -1,8 +1,29 @@
 const infosContainer = document.querySelector("#infos")
 const buttonOverview = document.querySelector("#see-overview")
 
-function consumirApi(){
-fetch("https://api.themoviedb.org/3/movie/popular", {
+const selectTv = document.querySelector("#search-select-tv")
+const selectMovie = document.querySelector("#search-select-movie")
+
+let movieSelect 
+
+[selectMovie, selectTv].forEach((el) => {
+  el.addEventListener("change", () => {
+    const value = el.value
+
+    if (el === selectMovie) {
+      movieSelect = true
+      consumirApi("movie", value)
+    } else {
+      movieSelect = false
+      consumirApi("tv", value)
+    }
+  })
+})
+
+function consumirApi(movieOrSerie, endPoint){
+  infosContainer.innerHTML = ""
+  let url = `https://api.themoviedb.org/3/${movieOrSerie}/${endPoint}`
+fetch(url, {
   headers: {
     Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDNiYTA0MmI4MWYzYmI3MzAxNzk2NTA5OGQ4MmE3MSIsIm5iZiI6MTc2MjU3NzQyMy4xNTIsInN1YiI6IjY5MGVjYzBmZTI0ZTM2M2ZmOTg3NDZjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mGs7YuHJled5n4pNlJMw-UPwAQcvbQ_qsEL_zjkbp5M",
     accept: "application/json"
@@ -10,40 +31,48 @@ fetch("https://api.themoviedb.org/3/movie/popular", {
 })
   .then(res => res.json())
   .then(data => data.results.forEach((result)=>{
+
     const backDropMovie = result.backdrop_path
     const overViewMovie = result.overview
     const posterMovie = result.poster_path
     const titleMovie = result.original_title
 
+    const titleSerie = result.name 
     const releaseDate = result.release_date
     const avgVote = result.vote_average
     const countVote = result.vote_count
     const adultContent = result.adult
-
     console.log(result)
 
+    if(movieSelect === true){
     infos(backDropMovie, posterMovie, overViewMovie, titleMovie)
     extraInfos(releaseDate, avgVote, countVote, adultContent)
-    
+    } 
+    else{
+    const releaseDate = result.first_air_date
+    infos(backDropMovie, posterMovie, overViewMovie, titleSerie)
+    extraInfos(releaseDate, avgVote, countVote, adultContent)
+    }
   }))
   .catch(err => console.error(err));
 }
-consumirApi()
 
 
-function infos(backDropMovie, posterMovie, overViewMovie, titleMovie){
+function infos(backDropMovie, posterMovie, overViewMovie, titleTvOrMovie){
   const divMovie = document.createElement("div")
   divMovie.id = "div-movie"
 
   const title = document.createElement("h1")
   title.classList.add("title-movie")
-  title.textContent = titleMovie
+  title.textContent = titleTvOrMovie
 
   const backDropImg = document.createElement("img")
+  backDropImg.alt = "img-backdrop"
   backDropImg.classList.add("img-backdrop")
   backDropImg.src = `https://image.tmdb.org/t/p/w500${backDropMovie}`
 
   const posterImg = document.createElement("img")
+  posterImg.alt = "img-poster"
   posterImg.classList.add("img-poster")
   posterImg.src = `https://image.tmdb.org/t/p/w500${posterMovie}`
 
@@ -86,7 +115,7 @@ function extraInfos(releaseDate, avgVote, countVote, adultContent){
 
   const liAdult = document.createElement("li")
   liAdult.classList.add("li-info")
-  liAdult.textContent = `${adultContent === false ? `This movie is for everyone` : `This movie is only for adult people`}`
+  liAdult.textContent = `${adultContent === false ? "This movie is for everyone" : "This movie is only for adult people"}`
 
   ulInfos.appendChild(liRelease)
   ulInfos.appendChild(liVote)
